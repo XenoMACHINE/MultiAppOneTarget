34a4ba7b8645dd39a138b004cfe72e467e37906a
Alexandre Ménielle <alexandre.menielle@gmail.com>
2019-04-14 13:22:16 +0200

    think complete model with multi viewcontroller useless (don't solve multi storyboard issue)

--- TargetTest/Controllers/Home/Overrides/App/HomeController-Bedague.swift
+++ TargetTest/Controllers/Home/Overrides/App/HomeController-Bedague.swift
@@ -1,7 +1,13 @@
 //  Created by Alexandre Ménielle on 13/04/2019.
 //Copyright © 2019 Alexandre Ménielle. All rights reserved.
 
+import UIKit
+
 class HomeController_Bedague : HomeController_ABL {
+    
+    override func getStoryboard() -> UIStoryboard {
+        return UIStoryboard(name: "HomeImage", bundle: nil)
+    }
 
     override func getAppName() -> String {
         return "Bedague"
