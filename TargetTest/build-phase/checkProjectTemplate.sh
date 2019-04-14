3baec6376f71a34ace5cd56b6a7ee2b9c1f1857f
Alexandre Ménielle <alexandre.menielle@gmail.com>
2019-04-14 13:55:22 +0200

    add models in script

--- TargetTest/build-phase/checkProjectTemplate.sh
+++ TargetTest/build-phase/checkProjectTemplate.sh
@@ -7,12 +7,16 @@ customerByERP+=(["Bedague"]="ABL" ["Berthelot"]="ABL" ["Bistrocash"]="Cambos" ["
 erps=("ABL" "Serca" "Odoo" "Prios" "Cambos")
 customers=("Bedague" "Berthelot" "Bistrocash" "Forez" "LEDG" "Milliet" "Ouest" "Pietrini" "PNB" "Rouquette" "Proxy")
 
+if [[ ${#customers[@]} != ${#customerByERP[@]} ]]; then
+    echo "customerByERP and customers array doesn't match !!"
+    exit N
+fi
+
+#Check Controllers
 cd TargetTest/Views
 for entry in `ls $search_dir`; do
-    echo "Entry : " $entry
 
     for customer in ${customers[@]}; do
-        #echo "Customer : "$customer "| ERP = "${customerByERP[$customer]}
 
         containsApp=0
         for file in `ls $entry/Overrides/App`; do
@@ -29,7 +33,7 @@ for entry in `ls $search_dir`; do
             customerErp=${customerByERP[$customer]}
 
             content=$(printf "\rimport Foundation\r\rclass "$entry"Controller_"$customer" : "$entry"Controller_"$customerErp" {\r\r}")
-            echo "Create "$customer" (customer)"
+            echo "Create "$customer" (customer) in Views/"$entry"/Overrides/App"
             echo $content > $entry/Overrides/App/$entry"Controller-"$customer".swift"
         fi
 
@@ -50,10 +54,67 @@ for entry in `ls $search_dir`; do
         #Create file in /ERP
         if [[ $containsERP == 0 ]]; then
             content=$(printf "\rimport Foundation\r\rclass "$entry"Controller_"$erp" : "$entry"Controller {\r\r}")
-            echo "Create "$erp" (ERP)"
+            echo "Create "$erp" (ERP) in Views/"$entry"/Overrides/ERP"
             echo $content > $entry/Overrides/ERP/$entry"Controller_"$erp".swift"
         fi
 
     done
 
 done #entries
+#END Check ViewControllers
+
+
+
+
+
+
+#Check Models
+cd ../Models
+for entry in `ls $search_dir`; do
+
+    for customer in ${customers[@]}; do
+
+        containsApp=0
+        for file in `ls $entry/Overrides/App`; do
+            customerName=${file//*-/}
+            customerName=${customerName//.swift/}
+
+            if [[ "$customer" == "$customerName" ]]; then
+                containsApp=1
+            fi
+        done # files
+
+        #Create file in /App
+        if [[ $containsApp == 0 ]]; then
+            customerErp=${customerByERP[$customer]}
+
+            content=$(printf "\rimport Foundation\r\rclass "$entry"Controller_"$customer" : "$entry"Controller_"$customerErp" {\r\r}")
+            echo "Create "$customer" (customer) in Models/"$entry"/Overrides/App"
+            echo $content > $entry/Overrides/App/$entry"Controller-"$customer".swift"
+        fi
+
+    done # customers
+
+    for erp in ${erps[@]}; do
+
+        containsERP=0
+        for file in `ls $entry/Overrides/ERP`; do
+            erpName=${file//*_/}
+            erpName=${erpName//.swift/}
+
+            if [[ "$erp" == "$erpName" ]]; then
+                containsERP=1
+            fi
+        done # files
+
+        #Create file in /ERP
+        if [[ $containsERP == 0 ]]; then
+            content=$(printf "\rimport Foundation\r\rclass "$entry"Controller_"$erp" : "$entry"Controller {\r\r}")
+            echo "Create "$erp" (ERP) in Models/"$entry"/Overrides/ERP"
+            echo $content > $entry/Overrides/ERP/$entry"Controller_"$erp".swift"
+        fi
+
+    done
+
+done #entries
+#END Check Models
